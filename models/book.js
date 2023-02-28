8c3ffb9ecb16e9ad357d7d21be8fc63ffdd2c224
ahmedalnimr <122454313+ahmedalnimr@users.noreply.github.com>
2023-02-28 14:57:32 +0000

    Add files via upload
    AA : new update

---
+++ models/book.js
@@ -0,0 +1,11 @@
+const mongoose = require(`mongoose`)
+
+// model = schema 
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    read: Boolean
+})
+
+module.exports.Book = mongoose.model(`Book`, bookSchema, `book`)
